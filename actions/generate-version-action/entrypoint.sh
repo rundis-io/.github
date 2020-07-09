aa221733ca4f686405157dc026dc8c920a58dc4c
Mike Whitehead <mwhitehead@nvidia.com>
2020-07-09 02:17:36 -0700

    set repo url to action input

--- actions/generate-version-action/entrypoint.sh
+++ actions/generate-version-action/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sh -c "echo $*"
-export CI_REPOSITORY_URL=$1
+export CI_REPOSITORY_URL=$INPUT_CI-REPOSITORY-URL
 /usr/bin/gen-semver > version
 echo "::set-output name=version::$(cat version)"
